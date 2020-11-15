601febeac14a12a8f3b04861b264d9b49f8b4b3e
sr_Adrover <sadrover@cifpfbmoll.eu>
2020-11-15 22:23:32 +0100

    feat: ufosPark dispatch creditCard

--- main.js
+++ main.js
@@ -1,35 +1,38 @@
-console.log("\n" + "Tarjeta de Abradolph" + "\n" + 
-"===================="        );
-console.log("Abradolph Lincler, 4916119711304546");
-console.log("\nOvni de Abradolph\n" + 
-"=================");
-console.log("Crédito de Abradolph: --> llamamos al metodo que calcula el credito" )
-console.log("\nAbradolph quiere otro ovni\n" + 
-"==========================");
-console.log("Su credito no ha cambiado: -->llamamos al crédito de abradolph");
-console.log("Ovni de Abradolph: --> metodo que devuelve ufo");
-console.log("\nLLega GearHead!\n" + 
-"===============");
-console.log("Su credito es cero: ");
-console.log("No puede reservar ovni: ");
-console.log("\nLLega Squanchy!\n" + 
-                             "==============");
-console.log("Squanchy, 4444444444444444");
-console.log("Su credito es: ");
-console.log("Su ovni es: ");
-console.log("\nAlgun ovni para Morty?\n=" + 
-"======================");
-console.log("Su credito no ha cambiado: ");
-console.log("No hay ovni Morty: ");
-console.log("\nFlota de ovnis\n" + 
-"==============");
-console.log("Packs\n");
-console.log("Credito de GearHead: ")
-console.log("\nLLega Squanchy!\n" + 
-"===============");
-console.log("\nLLega GearHead!\n" +
-"===============");
-console.log("\nLLega Birdpearson!\n" + 
-"==================");
-console.log("\nMorty quiere pack y ovni pero no quedan :(\n" + 
-"==========================================");
+// Creación de objetos
+
+var creditCard = {
+    owner : "Morty",
+    number : "123-456-789",
+    credit : 200,
+    symbol : "ADC",
+
+    pay : function(fee){
+        this.credit = this.credit - fee;
+    }
+}
+
+var ufosPark = {
+    fee : 50,
+    availabilityFleet : {
+        ufo1 : "",
+        ufo2 : "",
+        ufo3 : ""
+    },
+
+    dispatch : function dispatch(creditCard){
+        for(let ufo of Object.keys(this.availabilityFleet)){
+            if(ufo.valueOf != ""){
+                creditCard.pay(this.fee);
+                ufo.valueOf = creditCard.owner;
+            }
+        }
+        
+    }
+}
+console.log(creditCard);
+console.log(ufosPark);
+
+ufosPark.dispatch(creditCard);
+
+console.log(creditCard);
+console.log(ufosPark);

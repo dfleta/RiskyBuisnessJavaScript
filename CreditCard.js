82476141280884caa9192e175dbb9b55ac414b67
sr_Adrover <sadrover@cifpfbmoll.eu>
2020-11-23 20:15:00 +0100

    feat: Crear fichero CreditCard

---
+++ CreditCard.js
@@ -0,0 +1,12 @@
+function CreditCard(name, numberCard){
+    this.name = name;
+    this.numberCard = numberCard;
+    this.credit = 3000;
+    this.SYMBOL = "EZI";
+}
+
+CreditCard.prototype.pay = function(cost){
+    if (this.credit >= cost){
+        this.credit -= cost;
+    }
+}

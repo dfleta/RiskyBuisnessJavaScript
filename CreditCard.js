74bba6c90027445aef888a6990061a2d7d8448cc
sr_Adrover <sadrover@cifpfbmoll.eu>
2020-11-24 21:10:38 +0100

    docs: Añadir comentarios

--- CreditCard.js
+++ CreditCard.js
@@ -1,10 +1,18 @@
+/**
+ * Contructor  of CreditCard
+ * @param {User name} name 
+ * @param {User number} numberCard 
+ */
 function CreditCard(name, numberCard){
     this.name = name;
     this.numberCard = numberCard;
     this.credit = 3000;
     this.SYMBOL = "EZI";
 }
-
+/**
+ * To pay a service
+ * @param {Product cost} cost 
+ */
 CreditCard.prototype.pay = function(cost){
     if (this.credit >= cost){
         this.credit -= cost;

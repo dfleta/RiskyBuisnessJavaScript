719c3d0257a3d068bb4650a2fe9699b68c16590a
sr_Adrover <sadrover@cifpfbmoll.eu>
2020-11-25 00:12:15 +0100

    docs: Añadir comentarios

--- Receptive.js
+++ Receptive.js
@@ -1,7 +1,13 @@
+/**
+ * Constructor of Receptive
+ */
 function Receptive(){
     this.services = [];
 }
-
+/**
+ * Function to create instace if don't exist
+ * Apply the singleton pattern
+ */
 function singletonReceptive() {
 
     let instance;
@@ -19,15 +25,23 @@ function singletonReceptive() {
         }
     };
 };
-
+/**
+ * Add service to the list of availanility serivice
+ * @param {*} service 
+ */
 Receptive.prototype.registerService = function(service){
     this.services.push(service);
 }
-
+/**
+ * Dispath CreditCard with all added services 
+ * @param {CreditCard} creditCard 
+ */
 Receptive.prototype.dispatch = function(creditCard){
     for(service of this.services){
         service.dispatch(creditCard);
     }
 }
-
+/**
+ * Export de singleton function
+ */
 module.exports.singletonReceptive = singletonReceptive;
